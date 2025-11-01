132e51c2567111bc874ef9d3365faa787219315a
MirianMarriaga <mmmarriaga@unimagdalena.edu.co>
2025-11-01 10:17:09 -0500

    Agregadas vistas y plantilla para el detalle del post del blog así como también CSS para el sitio y algunos comentarios.

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'), 
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), #http://127.0.0.1:8000/post/5/ por ejemplo significara que se esta buscando una vista llamada post_detail y se le esta pasando un parametro pk con valor 5
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 #importante para renderizar plantillas y manejar errores 404
 from django.utils import timezone
 from .models import Post
 
-# Create your views here.
+# Views / Vistas
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html',  {'posts': posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #obtiene todos los objetos Post que tienen una fecha de publicacion menor o igual a la fecha y hora actual, y los ordena por fecha de publicacion
+    return render(request, 'blog/post_list.html',  {'posts': posts}) #renderiza la plantilla blog/post_list.html con el contexto que contiene los posts obtenidos
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk) #sirve para obtener un objeto o devolver un error 404 si no se encuentra
+    return render(request, 'blog/post_detail.html', {'post': post}) #renderiza la plantilla blog/post_detail.html con el contexto que contiene el post obtenido
